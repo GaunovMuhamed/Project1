160a63afce45ac34166bc486ae88b8b9ccd52e7b
GaunovMukhamed <gaunovmuhamed@gmail.com>
2020-02-14 17:55:22 +0300

    com

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
+import Checkboxes from './components/checkboxes'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
+    <Checkboxes />
   );
 }
 

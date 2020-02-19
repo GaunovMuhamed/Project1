2912ed36021f90012aeb45c88288cfed783b74b3
GaunovMukhamed <gaunovmuhamed@gmail.com>
2020-02-19 15:54:43 +0300

    added sideMenu component and transition blur on the big image of homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SignIn from './pages/SignIn/SignIn';
 import Register from './pages/Register/Register';
@@ -8,11 +8,13 @@ import Header from './components/Header/Header';
 import AboutUser from './pages/AboutUserPage/AboutUserPage';
 
 function App() {
+  const [finished, setFinished] = useState(false);
+
   return (
     <Router>
-      <Header />
+      <Header setFinished={setFinished} />
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/" render={() => <HomePage finished={finished} setFinished={setFinished} />} />
         <Route exact path="/signin" component={SignIn} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/allprojects" component={AllProjects} />

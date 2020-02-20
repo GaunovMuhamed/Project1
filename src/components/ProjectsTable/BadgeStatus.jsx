353e00f7a52b80952d2988e1b91c65400bc02a8c
Vladislav Senko <vladislavsenko98@gmail.com>
2020-02-20 19:44:12 +0300

    add badge

--- src/components/ProjectsTable/BadgeStatus.jsx
+++ src/components/ProjectsTable/BadgeStatus.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import Badge from '@material-ui/core/Badge';
 import NoteIcon from '@material-ui/icons/Note';
-import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
+import blue from '@material-ui/core/colors/blue';
+import { makeStyles } from '@material-ui/core';
 
 const useToolbarStyles = makeStyles(() => ({
-  active: {
-    color: 'primary',
+  title: {
+    flex: '1 1 20%',
   },
-  completed: {
-    color: 'error',
-  },
-  during: {
-    color: 'secondary',
+  search: {
+    flex: '1 1 100%',
+    justifyItems: 'center',
   },
 }));
 
-export default function BadgeStatus(projects) {
-  const classes = useToolbarStyles();
+export default function BadgeStatus(props) {
+  const styles = useStyles();
+  // const {large, small, tiny, active, pending, icon, completed, text } = props;
+  console.log('props', props);
 
+  // const badgeColor = clsx(styles.badge, {
+
+  //   succeed: completed,
+  //   action: active
+
+  // });
+  console.log(badgeColor);
   return (
-    <Badge color="primary" variant="dot">
-      <NoteIcon />
+    <Badge color={badgeColor} variant="dot">
+      {/* {icon}
+        {text} */}
     </Badge>
   );
 }
--- src/components/ProjectsTable/ProjectsList.jsx
+++ src/components/ProjectsTable/ProjectsList.jsx
@@ -3,24 +3,24 @@ import TableCell from '@material-ui/core/TableCell';
 import Checkbox from '@material-ui/core/Checkbox';
 import BadgeStatus from './BadgeStatus';
 
-export default function ProjectsList({ projects, isItemSelected, labelId }) {
+export default function ProjectsList(props) {
   return (
     <>
       <TableCell padding="checkbox">
         <Checkbox
-          checked={isItemSelected}
-          inputProps={{ 'aria-labelledby': labelId }}
+          checked={props.isItemSelected}
+          inputProps={{ 'aria-labelledby': props.labelId }}
         />
       </TableCell>
-      <TableCell component="th" id={labelId} scope="row" padding="none">
-        {projects.project}
+      <TableCell component="th" id={props.labelId} scope="row" padding="none">
+        {props.projects.project}
       </TableCell>
       <TableCell align="right">
-        <BadgeStatus projects={projects} />
+        <BadgeStatus projects={props.projects} />
       </TableCell>
-      <TableCell align="right">{projects.stack}</TableCell>
-      <TableCell align="right">{projects.developer}</TableCell>
-      <TableCell align="right">{projects.rait}</TableCell>
+      <TableCell align="right">{props.projects.stack}</TableCell>
+      <TableCell align="right">{props.projects.developer}</TableCell>
+      <TableCell align="right">{props.projects.rait}</TableCell>
     </>
   );
 }
